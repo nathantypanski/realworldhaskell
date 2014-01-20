9b2e4efd0b62c1325e7cbf20528e52781a801a40
Nathan Typanski <ntypanski@gmail.com>
2014-01-19 21:46:10 -0500

    ch04 exercise #4 primitive solution (pg. 84)

---
+++ ch04/ch04.exercises.4.hs
@@ -0,0 +1,21 @@
+--file: ch04/ch04.exercises.4.hs
+import System.Environment (getArgs)
+import Data.List (transpose)
+
+transposeText = unlines . transpose . lines
+
+interactWith :: (String -> String) -> FilePath -> FilePath -> IO ()
+interactWith function inputFile outputFile = do
+  input <- readFile inputFile
+  writeFile outputFile (function input)
+
+main :: IO ()
+main = mainWith myFunction
+  where mainWith function = do
+          args <- getArgs
+          case args of
+            [input,output] -> interactWith function input output
+            _ -> putStrLn "error: exactly two arguments needed"
+
+        -- replace "id" with the name of our function below
+        myFunction = transposeText
